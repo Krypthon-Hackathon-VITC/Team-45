894e611651ed9c4a50c7886ab4ed337424d6d423
depreeth <dc3016@srmist.edu.in>
2023-03-28 00:16:45 +0530

    testing on signup and login page

--- udecide/src/pages/login.js
+++ udecide/src/pages/login.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import img from "../assets/signup-bg.jpg";
 
-const login = () => {
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Make HTTP request to server-side endpoint to authenticate user
+    // fetch('/login', {
+    //   method: 'POST',
+    //   body: JSON.stringify({ email, password }),
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   }
+    // })
+    //   .then(response => response.json())
+    //   .then(data => {
+    //     if (data.success) {
+    //       // User is authenticated, do something
+    //       console.log('User authenticated');
+    //     } else {
+    //       // User is not authenticated, prompt to register
+    //       alert('Invalid email or password. Please register if you are not already a user.');
+    //     }
+    //   })
+    //   .catch(error => console.error(error));
+  };
+  console.log("username:", username);
+  console.log("password:", password);
+
   return (
     <div className=" grid grid-cols-1  xl:grid-cols-2 h-screen w-full">
       <div className="hidden xl:block rounded-lg">
@@ -9,17 +38,30 @@ const login = () => {
       </div>
       <div className=" bg-gray-100 flex flex-col justify-center rounded-lg">
         <form
-          action="/home"
+          //   action="/home"
+          onSubmit={handleSubmit}
           className=" max-w-[400px] w-full mx-auto bg-white p-4 rounded-xl"
         >
           <h2 className=" text-4xl text-center font-bold py-6">uDecide</h2>
           <div className=" flex flex-col py-2">
-            <label>Username</label>
-            <input className=" border p-2" type="text" />
+            <label htmlFor="username">Username</label>
+            <input
+              className=" border p-2"
+              type="text"
+              id="username"
+              value={username}
+              onChange={(event) => setUsername(event.target.value)}
+            />
           </div>
           <div className=" flex flex-col py-2">
-            <label>Password</label>
-            <input className=" border p-2" type="password" />
+            <label htmlFor="password">Password</label>
+            <input
+              className=" border p-2"
+              type="password"
+              id="password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
           </div>
           <button className=" border w-full my-5 py-2 bg-indigo-600 hover:bg-indigo-500 text-white">
             Sign In
@@ -39,4 +81,4 @@ const login = () => {
   );
 };
 
-export default login;
+export default Login;
--- udecide/src/pages/signup.js
+++ udecide/src/pages/signup.js
@@ -1,28 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import img1 from "../assets/login-bg.jpg";
 
-const signup = () => {
+const Signup = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const emailRegex = /^\S+@\S+\.\S+$/;
+    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+
+    if (!emailRegex.test(email)) {
+      alert('Please enter a valid email address.');
+      return;
+    }
+
+    if (!passwordRegex.test(password)) {
+      alert('Password must be at least 8 characters long and contain at least one letter and one number.');
+      return;
+    }
+
+    // Make HTTP request to server-side endpoint
+    //     fetch("/register", {
+    //       method: "POST",
+    //       body: JSON.stringify({ username, email, password }),
+    //       headers: {
+    //         "Content-Type": "application/json",
+    //       },
+    //     })
+    //       .then((response) => response.json())
+    //       .then((data) => console.log(data))
+    //       .catch((error) => console.error(error));
+
+    console.log("username:", username);
+    console.log("email:", email);
+    console.log("password:", password);
+  };
+
   return (
     <div className=" grid grid-cols-1 xl:grid-cols-2 h-screen w-full">
       <div className=" bg-gray-100 flex flex-col justify-center rounded-lg">
         <form
-          action="/home"
+          //   action="/home"
+          onSubmit={handleSubmit}
           className=" max-w-[400px] w-full mx-auto bg-white p-4 rounded-xl"
         >
           <h2 className=" text-4xl text-center font-bold py-6">uDecide</h2>
           <div className=" flex flex-col py-2">
-            <label>Username</label>
-            <input className=" border p-2" type="text" />
+            <label htmlFor="username">Username</label>
+            <input
+              className=" border p-2"
+              type="text"
+              id="username"
+              value={username}
+              onChange={(event) => setUsername(event.target.value)}
+            />
           </div>
           <div className=" flex flex-col py-2">
-            <label>Email</label>
-            <input className=" border p-2" type="email" />
+            <label htmlFor="email">Email</label>
+            <input
+              className=" border p-2"
+              type="email"
+              id="email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
           </div>
           <div className=" flex flex-col py-2">
-            <label>Password</label>
-            <input className=" border p-2" type="password" />
+            <label htmlFor="password">Password</label>
+            <input
+              className=" border p-2"
+              type="password"
+              id="password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
           </div>
-          <button className=" border w-full my-5 py-2 bg-indigo-600 hover:bg-indigo-500 text-white">
+          <button type="submit" className=" border w-full my-5 py-2 bg-indigo-600 hover:bg-indigo-500 text-white">
             Register
           </button>
           <div className=" flex justify-between">
@@ -43,4 +99,4 @@ const signup = () => {
   );
 };
 
-export default signup;
+export default Signup;
